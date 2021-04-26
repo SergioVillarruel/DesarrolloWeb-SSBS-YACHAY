b952b9ebde706bc56d90db82801dbfb4609226aa
giornogiovana <drakeb2008@hotmail.com>
2021-04-25 19:54:57 -0500

    First commit

---
+++ backend/src/main/java/com/yachay/YachayApplication.java
@@ -0,0 +1,13 @@
+package com.yachay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YachayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YachayApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/yachay/YachayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yachay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
